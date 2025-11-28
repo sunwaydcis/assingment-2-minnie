4447f2733e7a7fff9c65d8cb8efd1313f7773f2a
Thong Yong Ru <23021272@imail.sunway.edu.my>
2025-11-28 09:57:50 +0800

    Update HotelModels.scala

--- src/main/scala/HotelModels.scala
+++ src/main/scala/HotelModels.scala
@@ -1,26 +1,30 @@
+//HotelModels.scala
+
+//Case class representing a single hotel booking record
 case class HotelBooking (
-  hotel: String,
-  country: String,
-  bookingPrice: Double,
-  discount: Double,
-  profitMargin: Double,
-  visitors: Int
+  hotel: String, //Hotel name
+  country: String, //Country of origin
+  bookingPrice: Double, // Price in SDG
+  discount: Double, //Discount percentage
+  profitMargin: Double, //Profit margin (0-1)
+  visitors: Int //Number of people
 )
 
+//Case class representing aggregated statistics for a hotel
 case class HotelStats(
-  hotel: String,
-  totalBookings: Int,
-  totalVisitors: Int,
-  averagePrice: Double,
-  averageDiscount: Double,
-  averageProfitMargin: Double
+  hotel: String, //Hotel name
+  totalBookings: Int, //Total number of bookings
+  totalVisitors: Int, //Total number of visitors
+  averagePrice: Double, //Average booking price
+  averageDiscount: Double, //Average discount
+  averageProfitMargin: Double //Average profit margin
 )
 
-
+//Case class representing aggregated statistics for a country
 case class CountryStats(
-  country: String,
-  totalBookings: Int,
-  totalVisitors: Int,
-  averageBookingPrice: Double,
-  mostPopularHotel: String
+  country: String, //Country name
+  totalBookings: Int, //Total bookings from country
+  totalVisitors: Int, //Total visitors from country
+  averageBookingPrice: Double, //Average booking price
+  mostPopularHotel: String //Most booked hotel for this country
 )
